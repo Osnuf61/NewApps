abb068acde57470ba1c1eb27de869227236b3490
osnuf61 <o.s__laz@hotmail.com>
2018-11-15 11:03:27 +0100

    Just put some text in thats all

--- ExcellentTaste/Form1.cs
+++ ExcellentTaste/Form1.cs
@@ -16,5 +16,10 @@ namespace ExcellentTaste
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hello
+        }
     }
 }
